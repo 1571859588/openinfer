pegainfer-project/pegainfer

fix(observability): report prefix-cache counters as token-granular per-send deltas
Two review bugs on the prefix-cache `/metrics` surface, both fixed:

1. Unit mismatch (queries vs hits). Previously `prefix_queries += 1` (a
   request) while `prefix_hits += cached_tokens` (a token count), so
   `hit_rate = hits/queries` could exceed 100%. Now both are TOKEN-granular,
   matching vLLM's `PrefixCacheStats`: `prefix_queries` counts the prompt
   tokens looked up in the cache and `prefix_hits` counts the cached tokens.
   Because cached <= prompt, `hits <= queries` and the rate stays in [0, 1].
   Guarded by the existing `prefill_pos == 0` check so each request is counted
   exactly once (no double counting across chunked prefill).

2. Cumulative overcount in Prometheus. The scheduler holds running totals, but
   `dispatch_step` / `publish_scheduler_stats` shipped that running total on
   *every* token batch, and the frontend adds each `SchedulerStats` value into
   its `prefix_cache_*_total` counters — so a cached request re-added the whole
   history on every subsequent batch until restart. Now the bridge ships
   per-send DELTAS (cur - last), mirroring the existing spec-decode path:
   `prefix_cache_delta()` in bridge.rs, with `last_prefix_*` state in both the
   legacy `publish_scheduler_stats` loop and the stepped bridge (AtomicU64,
   since `dispatch_step` takes `&self`).

Adds a `FakeExecutor` prefix-hit hook, a multi-batch/multi-scrape qwen3
scheduler test (`prefix_cache_metrics_stable_across_batches_and_scrapes`,
token-granular assertions), and a frontend test
(`prefix_cache_stats_are_per_interval_deltas_not_running_totals`) that proves
the bridge ships the interval delta, not the running total.

Signed-off-by: yuntaonie <1571859588@qq.com>

pegainfer-frontend/src/vllm/bridge.rs
@@ -37,7 +37,6 @@ use vllm_engine_core_client::protocol::request::EngineCoreRequest;
 use vllm_engine_core_client::protocol::request::EngineCoreRequestType;
 use vllm_engine_core_client::protocol::stats::BaseCacheStats;
 use vllm_engine_core_client::protocol::stats::PrefillStats;
-use vllm_engine_core_client::protocol::stats::PrefixCacheStats;
 use vllm_engine_core_client::protocol::stats::SchedulerStats;
 use vllm_engine_core_client::protocol::stats::SpecDecodingStats;
 use vllm_engine_core_client::protocol::utility::UtilityCallId;
@@ -591,6 +590,14 @@ fn stop_sentinel_id(eos_token_id: Option<u32>, stop_token_ids: &[u32]) -> Option
 /// vLLM `SchedulerStats` view of a load snapshot — what the frontend's
 /// Prometheus gauges (`scheduler_running`, `scheduler_waiting`,
 /// `kv_cache_usage`) and DP load balancer consume.
+///
+/// `prefix_cache_stats` is left at zero here on purpose: the running totals in
+/// `SchedulerMetrics` must NOT be shipped as-is, because the frontend's
+/// Prometheus logger increments its `prefix_cache_*_total` counters by the
+/// value of *every* `SchedulerStats` it receives. The bridge therefore exports
+/// per-send **deltas** (see [`PrefixCacheTracker`]) so a cached request does
+/// not re-add the whole history on each subsequent token batch. Callers fill
+/// `prefix_cache_stats` from [`PrefixCacheTracker::interval`] after this call.
 pub(crate) fn scheduler_stats_from(snapshot: &SchedulerMetrics) -> SchedulerStats {
     SchedulerStats {
         num_running_reqs: snapshot.num_running_reqs,
@@ -600,15 +607,37 @@ pub(crate) fn scheduler_stats_from(snapshot: &SchedulerMetrics) -> SchedulerStat
         } else {
             snapshot.kv_used_blocks as f64 / snapshot.kv_total_blocks as f64
         },
-        prefix_cache_stats: PrefixCacheStats {
-            base: BaseCacheStats {
-                queries: snapshot.prefix_cache_queries,
-                hits: snapshot.prefix_cache_hits,
-                ..BaseCacheStats::default()
-            },
-            ..PrefixCacheStats::default()
-        },
         ..SchedulerStats::default()
+    }
+}
+
+/// Prefix-cache counterpart of [`SpecDecodeTracker`]: the scheduler holds
+/// running totals while the wire must carry per-interval deltas, because the
+/// frontend increments its `prefix_cache_*_total` counters by the value of
+/// *every* `SchedulerStats` it receives. Shipping the running total would
+/// re-add the whole history on each subsequent batch, so both bridges convert
+/// through this type and cannot drift.
+#[derive(Default)]
+pub(crate) struct PrefixCacheTracker {
+    last_queries: u64,
+    last_hits: u64,
+}
+
+impl PrefixCacheTracker {
+    /// The delta to stamp on the next outgoing batch. Advances the baseline, so
+    /// a caller that declines to send after calling this drops only a no-op
+    /// interval.
+    pub(crate) fn interval(&mut self, snapshot: &SchedulerMetrics) -> BaseCacheStats {
+        let delta = BaseCacheStats {
+            queries: snapshot
+                .prefix_cache_queries
+                .saturating_sub(self.last_queries),
+            hits: snapshot.prefix_cache_hits.saturating_sub(self.last_hits),
+            ..BaseCacheStats::default()
+        };
+        self.last_queries = snapshot.prefix_cache_queries;
+        self.last_hits = snapshot.prefix_cache_hits;
+        delta
     }
 }
 
@@ -672,9 +701,11 @@ async fn publish_scheduler_stats(
     shutdown: CancellationToken,
 ) -> Result<()> {
     let mut spec = SpecDecodeTracker::default();
+    let mut prefix = PrefixCacheTracker::default();
     loop {
         let snapshot = *load_rx.borrow_and_update();
         let mut stats = scheduler_stats_from(&snapshot);
+        stats.prefix_cache_stats.base = prefix.interval(&snapshot);
         stats.spec_decoding_stats = spec.interval(&snapshot);
         let outputs = RequestBatchOutputs {
             engine_index,

1571859588/openinfer

pegainfer-frontend/src/vllm/bridge.rs
@@ -37,7 +37,6 @@ use vllm_engine_core_client::protocol::request::EngineCoreRequest;
 use vllm_engine_core_client::protocol::request::EngineCoreRequestType;
 use vllm_engine_core_client::protocol::stats::BaseCacheStats;
 use vllm_engine_core_client::protocol::stats::PrefillStats;
-use vllm_engine_core_client::protocol::stats::PrefixCacheStats;
 use vllm_engine_core_client::protocol::stats::SchedulerStats;
 use vllm_engine_core_client::protocol::stats::SpecDecodingStats;
 use vllm_engine_core_client::protocol::utility::UtilityCallId;
@@ -592,6 +591,14 @@ fn stop_sentinel_id(eos_token_id: Option<u32>, stop_token_ids: &[u32]) -> Option
 /// vLLM `SchedulerStats` view of a load snapshot — what the frontend's
 /// Prometheus gauges (`scheduler_running`, `scheduler_waiting`,
 /// `kv_cache_usage`) and DP load balancer consume.
+///
+/// `prefix_cache_stats` is left at zero here on purpose: the running totals in
+/// `SchedulerMetrics` must NOT be shipped as-is, because the frontend's
+/// Prometheus logger increments its `prefix_cache_*_total` counters by the
+/// value of *every* `SchedulerStats` it receives. The bridge therefore exports
+/// per-send **deltas** (see [`PrefixCacheTracker`]) so a cached request does
+/// not re-add the whole history on each subsequent token batch. Callers fill
+/// `prefix_cache_stats` from [`PrefixCacheTracker::interval`] after this call.
 pub(crate) fn scheduler_stats_from(snapshot: &SchedulerMetrics) -> SchedulerStats {
     SchedulerStats {
         num_running_reqs: snapshot.num_running_reqs,
@@ -601,16 +608,69 @@ pub(crate) fn scheduler_stats_from(snapshot: &SchedulerMetrics) -> SchedulerStat
         } else {
             snapshot.kv_used_blocks as f64 / snapshot.kv_total_blocks as f64
         },
-        prefix_cache_stats: PrefixCacheStats {
-            base: BaseCacheStats {
-                queries: snapshot.prefix_cache_queries,
-                hits: snapshot.prefix_cache_hits,
-                ..BaseCacheStats::default()
-            },
-            ..PrefixCacheStats::default()
-        },
         ..SchedulerStats::default()
     }
+}
+
+/// Prefix-cache counterpart of [`SpecDecodeTracker`]: the scheduler holds
+/// running totals while the wire must carry per-interval deltas, because the
+/// frontend increments its `prefix_cache_*_total` counters by the value of
+/// *every* `SchedulerStats` it receives. Shipping the running total would
+/// re-add the whole history on each subsequent batch, so both bridges convert
+/// through this type and cannot drift.
+#[derive(Default)]
+pub(crate) struct PrefixCacheTracker {
+<<<<<<< HEAD
+    last_queries: u64,
+    last_hits: u64,
+=======
+    queries: u64,
+    hits: u64,
+    external_queries: u64,
+    external_hits: u64,
+>>>>>>> 9eed2740 (fixup! fix(observability): report prefix-cache counters as token-granular per-send deltas)
+}
+
+impl PrefixCacheTracker {
+    /// The delta to stamp on the next outgoing batch. Advances the baseline, so
+    /// a caller that declines to send after calling this drops only a no-op
+    /// interval.
+    pub(crate) fn interval(&mut self, snapshot: &SchedulerMetrics) -> BaseCacheStats {
+        let delta = BaseCacheStats {
+            queries: snapshot.prefix_cache_queries.saturating_sub(self.queries),
+            hits: snapshot.prefix_cache_hits.saturating_sub(self.hits),
+            ..BaseCacheStats::default()
+        };
+        self.queries = snapshot.prefix_cache_queries;
+        self.hits = snapshot.prefix_cache_hits;
+        delta
+    }
+    /// The same conversion for the external (connector) side — blocks restored
+    /// from CPU offload or over P2P rather than found in local KV. Kept on a
+    /// separate baseline so the two families cannot pollute each other, and
+    /// `None` when nothing was restored: a line with no connector leaves
+    /// `connector_prefix_cache_stats` unset instead of reporting zeros.
+    pub(crate) fn external_interval(
+        &mut self,
+        snapshot: &SchedulerMetrics,
+    ) -> Option<PrefixCacheStats> {
+        let base = BaseCacheStats {
+            queries: snapshot
+                .prefix_cache_external_queries
+                .saturating_sub(self.external_queries),
+            hits: snapshot
+                .prefix_cache_external_hits
+                .saturating_sub(self.external_hits),
+            ..BaseCacheStats::default()
+        };
+        self.external_queries = snapshot.prefix_cache_external_queries;
+        self.external_hits = snapshot.prefix_cache_external_hits;
+        (base.queries > 0 || base.hits > 0).then(|| PrefixCacheStats {
+            base,
+            ..PrefixCacheStats::default()
+        })
+    }
+>>>>>>> 9eed2740 (fixup! fix(observability): report prefix-cache counters as token-granular per-send deltas)
 }
 
 /// Per-interval spec-decode delta from two cumulative snapshots, in the wire
@@ -673,9 +733,11 @@ async fn publish_scheduler_stats(
     shutdown: CancellationToken,
 ) -> Result<()> {
     let mut spec = SpecDecodeTracker::default();
+    let mut prefix = PrefixCacheTracker::default();
     loop {
         let snapshot = *load_rx.borrow_and_update();
         let mut stats = scheduler_stats_from(&snapshot);
+        stats.prefix_cache_stats.base = prefix.interval(&snapshot);
         stats.spec_decoding_stats = spec.interval(&snapshot);
         let outputs = RequestBatchOutputs {
             engine_index,